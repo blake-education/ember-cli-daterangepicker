29ad2b0f108d0065b565dd3c63432123a9bbaff0
Julian Leviston <julian.leviston@blake.com.au>
2018-11-16 12:04:13 +1100

    Fix arguments issues eslint

--- tests/helpers/module-for-acceptance.js
+++ tests/helpers/module-for-acceptance.js
@@ -5,16 +5,16 @@ import destroyApp from '../helpers/destroy-app'
 
 export default function (name, options = {}) {
   module(name, {
-    beforeEach() {
+    beforeEach(...args) {
       this.application = startApp()
 
       if (options.beforeEach) {
-        return options.beforeEach.apply(this, arguments)
+        return options.beforeEach.apply(this, ...args)
       }
     },
 
-    afterEach() {
-      const afterEach = options.afterEach && options.afterEach.apply(this, arguments)
+    afterEach(...args) {
+      const afterEach = options.afterEach && options.afterEach.apply(this, ...args)
       return Promise.resolve(afterEach).then(() => destroyApp(this.application))
     },
   })
